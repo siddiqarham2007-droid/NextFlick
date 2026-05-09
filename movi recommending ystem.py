fa5aa26762574a1985fb42192ff59d48fcda405f
siddiqarham2007-droid <siddiqarham2007@gmail.com>
2026-05-09 12:09:27 +0530

    Update movi recommending ystem.py

--- movi recommending ystem.py
+++ movi recommending ystem.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pickle
 import pandas as pd
-
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 # ---------------- PAGE CONFIG ----------------
 st.set_page_config(
     page_title="NextFlick 🎬",
